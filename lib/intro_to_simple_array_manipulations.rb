ea14874ac869b16aa72e90594c8e46921025fc50
sunny-utility-4827 <mzakzook@me.com>
2019-09-03 18:09:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,4 +35,12 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
   
